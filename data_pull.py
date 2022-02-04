e95fee951f6d64ee55fdff49766ef064328e5a30
kevin <kevin.miller6720@gmail.com>
2022-02-04 10:29:33 -0600

    Added ability option to run in headless mode and parameters to allow downloading of files while in headless mode; set download path to user's download's folder

--- data_pull.py
+++ data_pull.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -7,11 +8,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # Get the location used by the manager and initialize the driver using Chrome
 service = Service(executable_path=ChromeDriverManager().install())
+
+# Specify options to run in headless mode for efficiency and to allow downloading of files while in headless mode
 options = Options()
-# options.headless = True
+options.add_argument("--headless")
+
+# Specify downloads folder
+home = str(Path.home())
+downloads_dir = home + r"\Downloads"
 
 # Open Chrome web browser
 driver = webdriver.Chrome(service=service, options=options)
+
+# Enable Chromium driver to download files while in headless mode
+params = {'behavior': 'allow', 'downloadPath': downloads_dir}
+driver.execute_cdp_cmd('Page.setDownloadBehavior', params)
 
 # Navigate to Redfin website
 driver.get("http://www.redfin.com")
@@ -25,7 +36,8 @@ driver.implicitly_wait(0.5)
 # Click the (Download All) link at the bottom of the results page to download a CSV with data from all real estate
 # listings for every page of the search results
 driver.find_element(By.ID, "download-and-save").click()
-driver.implicitly_wait(0.5)
+# driver.find_element(By.XPATH, "//a[@id='download-and-save']").click()
+driver.implicitly_wait(1.0)
 
 # Close browser
 driver.quit()
