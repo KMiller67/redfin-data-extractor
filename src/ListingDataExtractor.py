aabfdf6465d38d3b70fdca63bbc0b0f6e4345a33
kevinmiller <kevin.miller6720@gmail.com>
2022-05-04 21:59:51 -0500

    Renamed getListingData method to getData and added super downloadData and readDownloadData method calls; Commented out __init__ code as I don't think it will be needed

--- src/ListingDataExtractor.py
+++ src/ListingDataExtractor.py
@@ -1,14 +1,12 @@
-from selenium import webdriver
-
 from DataExtractor import DataExtractor
 from FilterMenu import FilterMenu
 
 
 class ListingDataExtractor(DataExtractor):
-    def __init__(self, driver: webdriver):
-        super().__init__(driver)
+    # def __init__(self):
+    #     super().__init__(self.driver)
 
-    def getListingData(self, search_criteria: str, home_types: list, time_on_redfin: str):
+    def getData(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
         super().searchLocation(search_criteria)
 
         filter_menu = FilterMenu(self.driver)
@@ -18,3 +16,8 @@ class ListingDataExtractor(DataExtractor):
         filter_menu.selectTimeOnRedfin(time_on_redfin)
         filter_menu.selectForeclosuresCheckbox()
         filter_menu.closeMenu()
+
+        super().downloadData()
+        super().readDownloadedData(delete_csv)
+
+        self.driver.quit()
