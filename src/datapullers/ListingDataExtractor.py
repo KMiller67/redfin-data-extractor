57432d936740ceac541cf30be52422e2139e82b7
kevinmiller <kevin.miller6720@gmail.com>
2022-05-16 21:35:30 -0500

    Methods changed to snake-case to be more pythonic; download_dir changed to download_directory to be more explicit

--- src/datapullers/ListingDataExtractor.py
+++ src/datapullers/ListingDataExtractor.py
@@ -1,28 +1,28 @@
 import time
 
 from selenium import webdriver
-from src.datapullers.DataExtractor import DataExtractor
-from src.FilterMenu import FilterMenu
+
+from DataExtractor import DataExtractor
+from src.pages.FilterMenu import FilterMenu
 
 
 class ListingDataExtractor(DataExtractor):
-    def __init__(self, driver: webdriver, download_dir: str, dictionary: dict[str, str]):
-        super().__init__(driver, download_dir)
-        self.dictionary = dictionary
+    def __init__(self, driver: webdriver, download_directory: str):
+        super().__init__(driver, download_directory)
 
-    def getData(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
+    def get_data(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
         super().go_to_homepage()
-        super().searchLocation(search_criteria)
+        super().search_location(search_criteria)
 
         filter_menu = FilterMenu(self.driver)
-        filter_menu.openMenu()
-        filter_menu.selectHomeTypes(home_types)
-        filter_menu.selectComingSoonCheckbox()
-        filter_menu.selectTimeOnRedfin(time_on_redfin)
-        filter_menu.selectForeclosuresCheckbox()
-        filter_menu.closeMenu()
+        filter_menu.open_menu()
+        filter_menu.select_home_types(home_types)
+        filter_menu.select_coming_soon_checkbox()
+        filter_menu.select_time_on_redfin(time_on_redfin)
+        filter_menu.select_foreclosures_checkbox()
+        filter_menu.close_menu()
 
-        super().downloadData()
+        super().download_data()
 
-        time.sleep(30)
-        super().readDownloadedData(delete_csv)
+        time.sleep(3)
+        super().read_downloaded_data(delete_csv)
--- src/datapullers/SalesDataExtractor.py
+++ src/datapullers/SalesDataExtractor.py
@@ -1,23 +1,28 @@
+import time
+
 from selenium import webdriver
+
 from src.datapullers.DataExtractor import DataExtractor
-from src.FilterMenu import FilterMenu
+from src.pages.FilterMenu import FilterMenu
 
 
 class SalesDataExtractor(DataExtractor):
-    def __init__(self, driver: webdriver, download_dir: str):
-        super().__init__(driver, download_dir)
+    def __init__(self, driver: webdriver, download_directory: str):
+        super().__init__(driver, download_directory)
 
-    def getData(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
+    def get_data(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
         super().go_to_homepage()
-        super().searchLocation(search_criteria)
+        super().search_location(search_criteria)
 
         filter_menu = FilterMenu(self.driver)
-        filter_menu.openMenu()
-        filter_menu.selectSoldData()
-        filter_menu.selectHomeTypes(home_types)
-        filter_menu.selectTimeOnRedfin(time_on_redfin)
-        filter_menu.selectForeclosuresCheckbox()
-        filter_menu.closeMenu()
+        filter_menu.open_menu()
+        filter_menu.select_sold_data()
+        filter_menu.select_home_types(home_types)
+        filter_menu.select_time_on_redfin(time_on_redfin)
+        filter_menu.select_foreclosures_checkbox()
+        filter_menu.close_menu()
 
-        super().downloadData()
-        super().readDownloadedData(delete_csv)
+        super().download_data()
+
+        time.sleep(3)
+        super().read_downloaded_data(delete_csv)
