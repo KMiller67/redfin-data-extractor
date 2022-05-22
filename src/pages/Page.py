6749c4af847ff4ac42e7fa71f05465ea71f6feb9
kevinmiller <kevin.miller6720@gmail.com>
2022-05-22 07:47:27 -0500

    Changed 'web_driver' attribute name to 'driver' to match other files

--- src/pages/Page.py
+++ src/pages/Page.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 
 class Page:
     def __init__(self, driver: webdriver):
-        self.web_driver = driver
+        self.driver = driver
 
     def find_element(self, xpath: str):
-        return self.web_driver.find_element(By.XPATH, xpath)
+        return self.driver.find_element(By.XPATH, xpath)
 
     def click(self, xpath: str):
         self.find_element(xpath).click()
