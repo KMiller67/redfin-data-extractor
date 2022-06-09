11e496fdd242b064d46089b4e356b3adfab59146
kevinmiller <kevin.miller6720@gmail.com>
2022-06-08 20:54:38 -0500

    Removed delete_csv as func parameters since downloading CSV is no longer necessary

--- RedfinDataExtractor.py
+++ RedfinDataExtractor.py
@@ -11,11 +11,11 @@ class RedfinDataExtractor:
         self.listing_data_extractor = ListingDataExtractor(self.driver, download_dir)
         self.sales_data_extractor = SalesDataExtractor(self.driver, download_dir)
 
-    def get_listing_data(self, search_criteria: str, home_types: list, time_on_redfin: str, delete_csv: bool):
-        return self.listing_data_extractor.get_data(search_criteria, home_types, time_on_redfin, delete_csv)
+    def get_listing_data(self, search_criteria: str, home_types: list, time_on_redfin: str):
+        return self.listing_data_extractor.get_data(search_criteria, home_types, time_on_redfin)
 
-    def get_sales_data(self, search_criteria: str, home_types: list, sold_within: str, delete_csv: bool):
-        return self.sales_data_extractor.get_data(search_criteria, home_types, sold_within, delete_csv)
+    def get_sales_data(self, search_criteria: str, home_types: list, sold_within: str):
+        return self.sales_data_extractor.get_data(search_criteria, home_types, sold_within)
 
     def close_browser(self):
         self.driver.quit()
